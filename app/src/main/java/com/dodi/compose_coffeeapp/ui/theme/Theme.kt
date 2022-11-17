7bd215a37c464e76a6561b4167c48ae4c8e3bbd2
Dodi <dodirivaldi64@gmail.com>
2022-11-17 20:48:11 +0700

    Finish

--- app/src/main/java/com/dodi/compose_coffeeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/dodi/compose_coffeeapp/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposeCoffeeAppTheme(
+fun JetCoffeeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
